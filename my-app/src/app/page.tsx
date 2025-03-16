b18eecfd9de08102d745418af51a3cb812b2a3ea
LugawPlain <troysarinas22@gmail.com>
2025-03-17 04:54:50 +0800

    perfected motion scale rombo

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -26,8 +26,8 @@ import HeroSection from "../../components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="w-full font-clashDisplayBold  bg-cover">
+    <div className="max-w-vw overflow-hidden  ">
+      <main className="w-full font-clashDisplayBold  bg-cover ">
         <HeaderNav></HeaderNav>
         <section className="w-full h-fit">
           <HeroSection></HeroSection>
